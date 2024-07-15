1f548ab89a6456bbaf885720c6cfd7e7ec7261e5
Davi Silva <davi.pimenta8@gmail.com>
2024-07-15 16:20:33 -0300

    Adding Clerk webhook URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhooks/user",
+]);
 
 export default clerkMiddleware(
   (auth, req) => {
